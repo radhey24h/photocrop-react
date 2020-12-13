c53b47f1f93c9ff1d3c4fd2d4a729505062937d1
Radheshyam Mishra <radheshyam.mishra@ihsmarkit.com>
2020-12-13 18:11:27 +0530

    changes on dashboard

--- src/screens/Dashboard/index.jsx
+++ src/screens/Dashboard/index.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 
 export default function Dashboard() {
-    return (
-        <div>
-            <Header />
 
+
+    const [userDetails, setuserDetails] = useState([]);
+
+    useEffect(() => {
+
+        let user = localStorage.getItem('userDetails')
+
+        setuserDetails(JSON.parse(user));
+        console.log(userDetails)
+    }, [])
+
+    return (
+        <>
+            <Header />
+            <div>
+                <div>{userDetails.email}</div>
+                <div>{userDetails.email}</div>
+                <div>{userDetails.email}</div>
+                <div>{userDetails.email}</div>
+                <div>{userDetails.email}</div>
+            </div>
             <Footer />
-        </div>
+        </>
     )
 }
